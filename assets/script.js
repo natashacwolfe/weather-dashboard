fc6f88a3c98e39e331caf929ad3d8021b12934a2
Natasha Wolfe <natasha.c.wolfe@gmail.com>
2020-01-13 14:45:33 -0800

    current temp, uv index functions adding values to the dom

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,10 @@
 let APIKey = "840bae92b2c263c3e01358649c74dfbf";
 
 
-function current(city){
+// $(document).ready(function(){
+//     console.log("document loaded");
+
+function displayCurrent(city){
     let queryURL = `http://api.openweathermap.org/data/2.5/weather?q= ${city} &appid=7ba67ac190f85fdba2e2dc6b9d32e93c&units=imperial`;
     $.ajax({
         url: queryURL,
@@ -10,16 +13,26 @@ function current(city){
     })
         .then(function(response){
             console.log(response);
+
+            let f = (response.main.temp * 9 / 5 + 32).toFixed(2);
+            let city = $("#city").text(response.name);
+            let currentTemp = $("#current-temp").text("Temperature: " + f);
+            let humidity = $("#current-humidity").text("Humidity: " + response.main.humidity);
+            let wind = $("#current-wind").text("Wind Speed: " + response.wind.speed);
+
+            let countryCode = response.sys.country;
             let lon = response.coord.lon;
             let lat = response.coord.lat;
-            uvIndex(lat, lon);
+
+            displayUvIndex(lat, lon);
+            displayFiveDay(city, countryCode);
         })
             .catch(function(error){
                 console.log(error)
             })
 }
 
-function fiveDayForecast(city){
+function displayFiveDay(city, countryCode){
     let queryURL = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${APIKey}&units=imperial`;
     $.ajax({
         url: queryURL,
@@ -28,13 +41,12 @@ function fiveDayForecast(city){
     }) 
         .then(function(response){
             console.log(response);
-            let city = $("#city").text(response.name);
-
+          
         })
 
 }
 
-function uvIndex(lat, lon){
+function displayUvIndex(lat, lon){
     let queryURL = `http://api.openweathermap.org/data/2.5/uvi?appid=${APIKey}&lat=${lat}&lon=${lon}`
     $.ajax({
         url: queryURL,
@@ -42,19 +54,20 @@ function uvIndex(lat, lon){
         dataType: "json"
     })  
         .then(function(response){
-            console.log(response);
-
+            // console.log(response);
+            let uvIndex = $("#uv-index").text("UV Index: " + response.value)
         })
 }
 
-
-
-
-$(".submit").on("click", function(event){
+function submitCity(event){
     event.preventDefault();
     
-    let city = $(".search").val();
+    let city = $(".search").val().trim();
     console.log(city);
-    current(city);
-    fiveDayForecast(city);
-})
+    displayCurrent(city);
+}
+
+$(".submit").on("click", submitCity)
+
+
+// })
