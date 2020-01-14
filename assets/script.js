6b5116fabbf55bd3d97627ca0090b0338677f78a
Natasha Wolfe <natasha.c.wolfe@gmail.com>
2020-01-14 14:37:46 -0800

    local storage set

--- assets/script.js
+++ assets/script.js
@@ -5,14 +5,14 @@ let APIKey = "840bae92b2c263c3e01358649c74dfbf";
 //     console.log("document loaded");
 
 function displayCurrent(city){
-    let queryURL = `http://api.openweathermap.org/data/2.5/weather?q= ${city} &appid=7ba67ac190f85fdba2e2dc6b9d32e93c&units=imperial`;
+    let queryURL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}&units=imperial`;
     $.ajax({
         url: queryURL,
         method: "GET", 
         dataType: "json"
     })
         .then(function(response){
-            console.log(response);
+            // console.log(response);
 
             let f = (response.main.temp * 9 / 5 + 32).toFixed(2);
             let city = $("#city").text(response.name);
@@ -25,29 +25,46 @@ function displayCurrent(city){
             let lat = response.coord.lat;
 
             displayUvIndex(lat, lon);
-            displayFiveDay(city, countryCode);
+            // displayFiveDay(lat, lon);
         })
             .catch(function(error){
                 console.log(error)
             })
 }
 
-function displayFiveDay(city, countryCode){
-    let queryURL = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${APIKey}&units=imperial`;
-    $.ajax({
-        url: queryURL,
-        method: "GET", 
-        dataType: "json"
-    }) 
-        .then(function(response){
-            console.log(response);
-          
-        })
+// function displayFiveDay(lat, lon){
+//     let queryURL = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIKey}&units=imperial`
+//     $.ajax({
+//         url: queryURL,
+//         method: "GET", 
+//         dataType: "json"
+//     }) 
+//         .then(function(response){
+//             console.log(response);
+//             let forecastHeading = $("$five-day-h2").text("5-Day Forcast");
+//                 for(let i = 0; i <= 5; i++){
+//                     let fiveDayTemp = $("<p>").text(response.list[i].main.temp);
+//                     let weatherIconCode = response.list[i].weather[0].icon;
+//                     let weatherIconUrl = $("<img>").attr("src", `http://openweathermap.org/img/w/`)
+//                     let unixTimeStamp = response.list[i].dt;
+//                     let convertedDate = moment.unix(unixTimeStamp).utc().format("MM-DD");
+//                     console.log(convertedDate)
+//                     let fiveDayDateText = $("<h5>").text(convertedDate);
+//                     let 
+                    
 
-}
+//                 }
+
+             
+            
+
+         
+//         })
+
+// }
 
 function displayUvIndex(lat, lon){
-    let queryURL = `http://api.openweathermap.org/data/2.5/uvi?appid=${APIKey}&lat=${lat}&lon=${lon}`
+    let queryURL = `http://api.openweathermap.org/data/2.5/uvi?appid=${APIKey}&lat=${lat}&lon=${lon}&cnt=5`
     $.ajax({
         url: queryURL,
         method: "GET", 
@@ -61,10 +78,16 @@ function displayUvIndex(lat, lon){
 
 function submitCity(event){
     event.preventDefault();
-    
     let city = $(".search").val().trim();
     console.log(city);
     displayCurrent(city);
+    setSearchHistory(city)
+}
+
+
+function setSearchHistory(city){
+    let savedCity = localStorage.setItem("savedCity",JSON.stringify((city))) || [];
+    savedCity.unshift(city)
 }
 
 $(".submit").on("click", submitCity)
